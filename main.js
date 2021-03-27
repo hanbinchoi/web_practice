def5720b1a9235e753eb5402f862376e15027b1b
hanbinchoi <wkghskak@naver.com>
2021-03-27 15:50:49 +0900

    210327

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
-function templateHTML(_title, _list, body){
+function templateHTML(title, list, body, control){
   return   `<!doctype html>
     <html>
     <head>
-      <title>WEB1 - ${_title}</title>
+      <title>WEB1 - ${title}</title>
       <meta charset="utf-8">
     </head>
     <body>
       <h1><a href="/">WEB</a></h1>
-      ${_list}
+      ${list}
+      ${control}
       ${body}
     </body>
     </html>
@@ -33,36 +35,149 @@ var app = http.createServer(function(request,response){
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
-
       if(queryData.id === undefined){
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
-          var description = '홈페이지 입니다.';
+          var description = 'Hello, Node.js';
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>
-          <p>${description}</p>`);
+          var template = templateHTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`);
 
           response.writeHead(200);
           response.end(template);
-        })
+        });
 
 
 
       } else {
 
-        fs.readdir('data/', 'utf8', function(err, filelist){
+        fs.readdir('./data', 'utf8', function(err, filelist){
 
           fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
             var title = queryData.id;
             var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>
-            <p>${description}</p>`);
+            var template = templateHTML(title, list,
+              `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>
+            <a href="/update?id=${title}">update</a>
+            <form action="delete_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+            </form>`);
             response.writeHead(200);
             response.end(template);
           });
 
         });
       }
+    }
+    else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - create';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+          <form action="/create_process" method="post">
+
+            <p><input type="text" name="title" placeholder='title' ></p>
+            <p>
+              <textarea name="description" placeholder='description'></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `,'');
+
+        response.writeHead(200);
+        response.end(template);
+      })
+    }
+    else if(pathname === '/create_process'){ //생성 기능
+      var body = '';
+      request.on('data',function(data){
+        body = body + data;
+
+      });//웹 브라우저가 host방식으로 데이터를 전송할때 엄청 많으면 한번에 처리하면 무리가 간다.
+      // host방식으로 전송되는 데이터가 많을때 대비해서 사용
+      request.on('end',function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
+          fs.writeFile(`data/${title}`,description,'utf8', function(err){
+            response.writeHead(302, {Location:`/?id=${title}`});
+            response.end('success');
+          })
+      }); // 정보가 조각조각 들어오다가 들어올 정보가 없으면 이 함수가 호출됨
+
+    }
+
+    else if(pathname === '/update'){
+      fs.readdir('./data', 'utf8', function(err, filelist){
+        fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list,
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder='title' value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder='description'>${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+          `,
+        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+      );
+          response.writeHead(200);
+          response.end(template);
+        });
+
+      });
+    }
+
+    else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data',function(data){
+        body = body + data;
+
+      });//웹 브라우저가 host방식으로 데이터를 전송할때 엄청 많으면 한번에 처리하면 무리가 간다.
+      // host방식으로 전송되는 데이터가 많을때 대비해서 사용
+      request.on('end',function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`,`data/${title}`,function(error){
+            fs.writeFile(`data/${title}`,description,'utf8', function(err){
+              response.writeHead(302, {Location:`/?id=${title}`});
+              response.end('success');
+            })
+          });
+
+      }); // 정보가 조각조각 들어오다가 들어올 정보가 없으면 이 함수가 호출됨
+
+    }
+
+    else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data',function(data){
+        body = body + data;
+
+      });//웹 브라우저가 host방식으로 데이터를 전송할때 엄청 많으면 한번에 처리하면 무리가 간다.
+      // host방식으로 전송되는 데이터가 많을때 대비해서 사용
+      request.on('end',function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          fs.unlink(`data/${id}`, function(error){
+            response.writeHead(302, {Location:`/`});
+            response.end('success');
+          })
+      }); // 정보가 조각조각 들어오다가 들어올 정보가 없으면 이 함수가 호출됨
+
     }
 
     else {
